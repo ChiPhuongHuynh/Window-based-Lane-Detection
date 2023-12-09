edf8cdfdb12e40a1c14079068a12489014d79a8a
FandMChiHuynh <90219828+FandMChiHuynh@users.noreply.github.com>
2023-12-08 20:25:00 -0600

    added testing

--- cvbased-window.py
+++ cvbased-window.py
@@ -40,10 +40,18 @@ def midlane_coordinates(frame):
     #print(right_x)
 
     mid_value = (histogram[left_x,2] + histogram[right_x,2]) / 2
+
     if (mid_value > 20000):
+        print(left_x)
+        print(right_x)
         return (left_x + right_x) / 2
 
     return -1
+
+def midlane_draw(frame, mid):
+    cv2.line(frame,(int(mid-50),0),(int(mid-50),640),(255,0,0),5)
+    cv2.line(frame, (int(mid+50),0),(int(mid+50),640),(255,0,0),5)
+    return frame
 
 def pipeline(filename):
     #filename = "nonperturbed1.png"
@@ -55,53 +63,23 @@ def pipeline(filename):
     mid = midlane_coordinates(frame)
     return mid
 
-def sliding_window(frame, left_l, right_l):
-    #contours is the shifting of white pixels
-    lx = []
-    rx = []
-    y = 479
-
-    while y > 0:
-        img = frame[y-40:y, left_l-50:left_l+50]
-        contours,h = cv2.findContours(img, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        for c in contours:
-            m = cv2.moments(c)
-            if(m["m00"] != 0):
-                cx = int(m["m10"] / m["m01"]) #center of the contours
-                left_l = left_l - 50 + cx
-                lx.append(left_l)
-
-        img = frame[y-40:y, right_l-50:right_l+50]
-        contours,h = cv2.findContours(img, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        for c in contours:
-            m = cv2.moments(c)
-            if(m["m00"] != 0):
-                cx = int(m["m10"] / m["m00"]) #center of the contours
-                right_l = right_l - 50 + cx
-                rx.append(right_l)
-        midx = int((right_l+left_l)/2)
-
-        cv2.rectangle(frame, (midx-25,y), (midx+25,y-40), (255,255,255), 1)
-        y = y - 40
-    return frame
-
-def midlane_draw(frame, mid):
-    img = np.zeros((512,512,3), np.uint8)
-    cv2.line(img,(0,0),(511,511),(255,0,0),5)
-    frame = img
-    return frame
-
 def main():
     filename = "nonperturbed1.png"
-
     img = cv2.imread(filename)
-    frame = cv2.resize(img, (640,480))
+    img = cv2.resize(img, (640,480))
+    tf = transformed_frame(img)
+    mask = masking(tf)
+    frame = threshold(tf,mask)
+
 
     mid = pipeline(filename)
+    frame= midlane_draw(frame, mid)
+    cv2.imshow('original',img)
+    cv2.imshow('transformed_frame',tf)
+    cv2.imshow('Mask',mask)
+    cv2.imshow('Midlane Outline',frame)
 
-    """
-    cv2.imshow('contours',frame)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()"""
+    cv2.destroyAllWindows()
 
 main()
